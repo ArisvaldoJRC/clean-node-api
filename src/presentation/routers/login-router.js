763b4fba040b7e1f6e394eb7b22fa04825973d9f
Jose Arisvaldo R Clemente <ariclemente@policiacivil.pe.gov.br>
2020-07-01 13:40:46 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -6,7 +6,7 @@ export default class LoginRouter {
   }
 
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) {
+    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
       return HttpResponse.serverError()
     }
     const { email, password } = httpRequest.body
