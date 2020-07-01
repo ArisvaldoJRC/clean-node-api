494a96b8b934812ef314cd974908b8e138cb8ade
Jose Arisvaldo R Clemente <ariclemente@policiacivil.pe.gov.br>
2020-07-01 13:59:43 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/helpers/http-reponse.js
+++ src/presentation/helpers/http-reponse.js
@@ -21,9 +21,10 @@ export default class HttpResponse {
     }
   }
 
-  static ok () {
+  static ok (data) {
     return {
-      statusCode: 200
+      statusCode: 200,
+      body: data
     }
   }
 }
