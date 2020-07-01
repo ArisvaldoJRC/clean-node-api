98629cd440a736b932953bcd9c41da8d706d5b14
Jose Arisvaldo R Clemente <ariclemente@policiacivil.pe.gov.br>
2020-07-01 11:02:44 -0300

    ensure LoginRouter return 400 if no email is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,23 @@
-describe('', () => {
-  it('', () => {
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
 
+describe('Login Router', () => {
+  it('Should return 400 if no email is provided', () => {
+    // sut - system under test
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
