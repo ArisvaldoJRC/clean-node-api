197e2b348959a36655dd1708c0c642a35f6412c4
Jose Arisvaldo R Clemente <ariclemente@policiacivil.pe.gov.br>
2020-07-05 20:09:22 -0300

    ensure Encrypter calls bcrypt with correct values

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,6 +1,10 @@
 export default {
   isValid: true,
+  value: '',
+  hash: '',
   compare (value, hash) {
+    this.value = value
+    this.hash = hash
     return this.isValid
   }
 }
--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -10,14 +10,21 @@ class Encrypter {
 describe('Encrypter', () => {
   it('Should return true if bcrypt returns true', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_value', 'hash_value')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   it('Should return false if bcrypt returns false', async () => {
     const sut = new Encrypter()
     bcrypt.isValid = false
-    const isValid = await sut.compare('any_value', 'hash_value')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  it('Should call bcrypt with correct values', async () => {
+    const sut = new Encrypter()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
